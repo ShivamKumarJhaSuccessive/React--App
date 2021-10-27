24988ebbb8cd418f6cc032d512443d20df6830e0
Shivam Kumar Jha <shivam.jha@successive.tech>
2021-10-27 19:06:24 +0530

    Navbar and Login UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
-/* eslint-disable import/named */
-/* eslint-disable import/extensions */
-/* eslint-disable import/no-unresolved */
-export { default as TextFieldDemo } from './TextFieldDemo';
+export { TextFieldDemo } from './TextFieldDemo';
 export { default as InputDemo } from './InputDemo';
-export { default as Trainee } from './Trainee';
-export { default as ChildrenDemo } from './ChildrenDemo';
+export { Trainee } from './Trainee';
+export { ChildrenDemo } from './ChildrenDemo';
 export { default as Navbar } from './components';
 export { default as Login } from './Login';
