44520d132d4b9d2b5e60b6a6cc79412d5f1b38dd
Shivam Jha <shivam.jha@shivamr108.successivetech.com>
2021-12-15 13:16:19 +0530

    Pagination in Table

--- src/pages/Trainee/TraineeList.jsx
+++ src/pages/Trainee/TraineeList.jsx
@@ -1,17 +1,20 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useState, useEffect, useRef } from 'react';
-// import { useHistory } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { StickyHeadTable } from '../../components';
-import { AddDialog } from './components';
+import { AddDialog, EditDialog, RemoveDialog } from './components';
+import { getDateFormatted, toUpperCase } from '../../lib/utils/math';
 import { trainees } from './data';
-import { toUpperCase, getDateFormatted } from '../../lib/utils/math';
 
 const tableData = [
+
   {
     field: 'name',
     label: 'Name',
-    align: 'center',
+    align: 'left',
   },
   {
     field: 'email',
@@ -19,6 +22,7 @@ const tableData = [
     format: toUpperCase,
     align: 'left',
   },
+
   {
     field: 'createdAt',
     label: 'Date',
@@ -29,19 +33,12 @@ const tableData = [
 
 const TraineeList = () => {
   // Trainee Data
-  const [traineesData, setTraineesData] = useState({
-    loading: false,
-    traineeArr: [],
-    dataLength: 0,
-  });
   const [values, setValues] = useState({
     _id: '',
+    originalId: '',
     name: '',
     email: '',
   });
-
-  // const history = useHistory();
-  const [loader, setLoader] = useState(false);
 
   const timer = useRef();
 
@@ -49,56 +46,108 @@ const TraineeList = () => {
     clearTimeout(timer.current);
   }, []);
 
-  const fetchData = async () => {
-    setTraineesData({ loading: false, traineeArr: trainees, dataLength: trainees.length });
+  // Sort
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('');
+
+  const handleSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
   };
 
-  useEffect(() => {
-    try {
-      (async () => {
-        await fetchData();
-      })();
-    } catch (error) {
-      return error.response.data.message
-        && setValues({ ...values, err: error.response.data.message, success: '' });
-    }
-    return () => {};
-  }, []);
+  // Pagination
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
-  // Add Dialog
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  // Add Dialog Edit Dialog remove Dialog
   const [openAddDialog, setOpenAddDialog] = useState(false);
+  const [openEditDialog, setOpenEditDialog] = useState(false);
+  const [openRemoveDialog, setOpenRemoveDialog] = useState(false);
 
   const handleAddDialogOpen = () => {
     setOpenAddDialog(true);
     setValues({
+      _id: '',
+      originalId: '',
       name: '',
       email: '',
+      createdAt: '',
     });
   };
 
   const handleAddDialogClose = () => {
     setOpenAddDialog(false);
     setValues({
+      _id: '',
+      originalId: '',
       name: '',
       email: '',
+      createdAt: '',
     });
   };
 
-  const handleOnAddSubmit = async () => {
-    try {
-      setLoader(true);
-      setTraineesData({ loading: false, traineeArr: trainees, dataLength: trainees.length });
-      handleAddDialogClose();
-    } catch (error) {
-      return error.response.data.message
-      && setValues({ ...values, err: error.response.data.message, success: '' });
-    } finally {
-      timer.current = setTimeout(() => {
-        setLoader(false);
-      }, 1000);
-    }
-    return false;
+  const handleEditDialogOpen = (item) => {
+    setOpenEditDialog(true);
+    setValues({
+      _id: item._id,
+      originalId: item.originalId,
+      name: item.name,
+      email: item.email,
+      createdAt: item.createdAt,
+    });
   };
+  const handleOnEditClose = () => {
+    setOpenEditDialog(false);
+    setValues({
+      _id: '',
+      originalId: '',
+      name: '',
+      email: '',
+      createdAt: '',
+    });
+  };
+
+  const handleRemoveDialogOpen = (item) => {
+    setOpenRemoveDialog(true);
+    setValues({
+      _id: item._id,
+      name: item.name,
+      email: item.email,
+      createdAt: item.createdAt,
+    });
+  };
+
+  const handleOnRemoveClose = () => {
+    setOpenRemoveDialog(false);
+    setValues({
+      _id: '',
+      name: '',
+      email: '',
+      createdAt: '',
+    });
+  };
+
+  // Actions
+  const tableActions = [
+    {
+      icon: <EditIcon fontSize="small" />,
+      handler: handleEditDialogOpen,
+    },
+    {
+      icon: <DeleteIcon fontSize="small" />,
+      handler: handleRemoveDialogOpen,
+    },
+  ];
 
   return (
     <div>
@@ -115,19 +164,46 @@ const TraineeList = () => {
       </Button>
       {openAddDialog && (
         <AddDialog
-          loader={loader}
           open={openAddDialog}
           onClose={handleAddDialogClose}
-          onSubmit={(e) => handleOnAddSubmit(e, values)}
           errorMessage={values.err}
           successMessage={values.success}
         />
       )}
       <StickyHeadTable
         id="table1"
-        data={traineesData.traineeArr}
+        data={trainees}
         columns={tableData}
+        actions={tableActions}
+        orderBy={orderBy}
+        order={order}
+        onSort={handleSort}
+        compPath="/trainee"
+        rowsPerPageOptions={[5, 10, 25, 50, 100]}
+        count={trainees.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
       />
+      {openEditDialog && (
+        <EditDialog
+          idValue={values.originalId}
+          nameValue={values.name}
+          emailValue={values.email}
+          open={openEditDialog}
+          onClose={handleOnEditClose}
+          // onSubmit={(e) => handleOnEditSubmit(e, values)}
+        />
+      )}
+      {openRemoveDialog && (
+        <RemoveDialog
+          dateValue={values.createdAt}
+          open={openRemoveDialog}
+          onClose={handleOnRemoveClose}
+          // onSubmit={() => handleOnRemoveSubmit(values)}
+        />
+      )}
     </div>
   );
 };
